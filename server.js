9ae0f3114fd9d0e034bcefc9ba3b5065e6dcb666
Rohan Sood <rohansood42@hotmail.com>
2016-08-09 22:44:19 +0000

    Completed

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 var express = require('express');
+var accepts = require('accepts');
+var platform = require('platform');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var ip;
+  if (req.headers['x-forwarded-for']) {
+    ip = req.headers['x-forwarded-for'].split(",")[0];
+  } else if (req.connection && req.connection.remoteAddress) {
+    ip = req.connection.remoteAddress;
+  } else {
+    ip = req.ip;
+  }
+  var locale = accepts(req).languages();
+  //console.log(platform.os.toString());
+  
+  var retobj = {
+    ipaddress:ip,
+    language:locale[0],
+    software:platform.os.toString()
+  };
+  res.send(retobj);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Example app listening on port 8080!');
 });
